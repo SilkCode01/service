82bf5c7cd01370ce2d126c8961b8a018a176101b
DariusAndreiParaschiv <72765284+DariusAndreiParaschiv@users.noreply.github.com>
2022-03-16 21:30:14 +0200

    parsed data

---
+++ src/main/java/ro/unibuc/hello/Menu.java
@@ -0,0 +1,36 @@
+package ro.unibuc.hello;
+
+import org.springframework.boot.SpringApplication;
+
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int option;
+        int exit = 0;
+        System.out.println("SilkCode Production\n");
+        while(true){
+            System.out.println("Type the number of your option then press enter:\n" +
+                    "1.Show recent\n" +
+                    "2.Search cryptocurrency\n" +
+                    "0.Exit");
+            option = input.nextInt();
+            switch(option){
+                case 1:
+                    System.out.println("Feature not yet available");
+                    break;
+                case 2:
+                    System.out.println("Feature not yet available");
+                    break;
+                case 0:
+                    exit = 1;
+                    break;
+                default:
+                    System.out.println("Please enter a valid number");
+            }
+            if(exit == 1)
+                break;
+        }
+    }
+}
